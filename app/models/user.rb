c4747b6d72da03c6ce56d0408cdc9235b01e1109
Max Lund <mlund@billypayments.com>
2017-09-14 14:32:22 -0500

    user model -> add validations and associations
    -name, role required
    -email, phone format and require either
    -has many associations for shifts and managed_shifts
    -clean phone number prior to validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+	validates_presence_of :name, :role
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Invalid email" }, allow_blank: true
+	validates :phone, format: { with: /\A[0-9]{10}/, message: "Invalid Phone Number, must be 10 digits"}, allow_blank: true
+	validate :validate_presence_of_phone_or_email
+
+	before_validation :clean_phone_number
+
+	enum role: [:employee, :manager]
+	has_many :shifts, :foreign_key => :employee_id
+	has_many :managed_shifts, :class_name => "Shift", :foreign_key => :manager_id
+
+	private
+
+	def validate_presence_of_phone_or_email
+		errors.add(:base, "Phone or Email must be present") unless phone.present? or email.present?
+	end
+
+	def clean_phone_number
+		if phone.present? and phone_changed?
+			self.phone.gsub!(/[\s\-\(\)]/, '') if self.phone.gsub(/[\s\-\(\)]/, '')
+		end
+	end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+	context "validations" do
+		it { should validate_presence_of(:name) }
+		it { should validate_presence_of(:role) }
+		it { should allow_value("test@mail.com").for(:email) }
+		it { should_not allow_value("not-an_email").for(:email) }
+		it { should allow_value("1233211234").for(:phone) }
+		it { should_not allow_value("123321").for(:phone) }
+		it { should_not allow_value("tenletters").for(:phone) }
+		it { should_not allow_value("tenLet1232").for(:phone) }
+		it "should raise error without presence of phone or email" do
+			expect{create(:user, :role => :employee)}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Phone or Email must be present")
+		end
+		it "should validate presence of phone or email" do
+			expect{create(:user, :role => :employee, :phone => "1233211234")}.to_not raise_error
+			expect{create(:user, :role => :employee, :email => "test@test.com")}.to_not raise_error
+		end
+		it "should remove white space and delimiters of phone number before validation" do
+			user = create(:user, :role => :employee, :phone => "(123) 342-1221")
+			expect(user.phone).to eq("1233421221")
+		end
+	end
+
+	context "associations" do
+		it { should have_many(:shifts) }
+		it { should have_many(:managed_shifts) }
+	end
+
+	context "enums" do
+		it { should define_enum_for(:role).with(:employee => 0, :manager => 1) }
+	end
+	
+end
