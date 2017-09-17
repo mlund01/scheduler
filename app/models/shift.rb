bd2cc6d9c3793db3f0c97621614ef73ca6049bed
Max Lund <mlund@billypayments.com>
2017-09-17 18:57:46 -0500

    shift model -> add assigned_to scope and validate end_time is a later time than start_time

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,6 +1,17 @@
 class Shift < ApplicationRecord
 	validates_presence_of :start_time, :end_time, :manager_id
+	validate :end_time_must_be_later_than_start_time
 
 	belongs_to :manager, :class_name => "User"
 	belongs_to :employee, :class_name => "User", optional: true
+
+	scope :assigned_to, -> (id) { where(:employee_id => id) }
+
+	private
+
+	def end_time_must_be_later_than_start_time
+		if start_time.to_i > end_time.to_i
+			errors.add(:end_time, "must be set to a later time than start_time")
+		end
+	end
 end
--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -12,10 +12,30 @@ RSpec.describe Shift, type: :model do
 		it "should allow employee_id to be blank" do
 			expect{create(:shift, :manager_id => @manager.id)}.to_not raise_error
 		end
+		it "should raise error if start_time is set later than end_time" do
+			expect{
+				create(:shift, 
+					:manager_id => @manager.id,
+					:start_time => Time.new + 1.day + 15.minutes,
+					:end_time => Time.new + 1.day
+					)
+			}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: End time must be set to a later time than start_time")
+		end
 	end
 
 	context "associations" do
 		it { should belong_to(:employee) }
 		it { should belong_to(:manager) }
+	end
+
+	context "scopes" do
+		it "should only return shifts assigned to employee with assigned_to" do
+			employee_2 = create(:user, :role => :employee, :phone => '3332221111')
+			create(:shift, :manager_id => @manager.id, :employee_id => @employee.id)
+			create(:shift, :manager_id => @manager.id, :employee_id => @employee.id)
+			create(:shift, :manager_id => @manager.id, :employee_id => employee_2.id)
+			employee_shifts = Shift.assigned_to(@employee.id)
+			expect(employee_shifts.count).to eq(2)
+		end
 	end
 end
