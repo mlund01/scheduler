1835b5290dddf6301c82264781d26dbe652a6c47
Max Lund <mlund@billypayments.com>
2017-09-18 11:13:56 -0500

    config -> timezone to America/Chicago and debug settings to STDOUT (for docker)

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ module Scheduler
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = 'America/Chicago'
+
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
