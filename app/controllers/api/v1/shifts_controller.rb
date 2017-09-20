50821a0493212cc9dfd33fdb513d6e2b99cb7d10
Max Lund <mlund@billypayments.com>
2017-09-20 14:49:02 -0500

    Shifts Controller -> add role scopes and query capabilities
    - scope param for listing assigned or unassigned shifts
    - default manager_id to current manager for new shift

--- app/controllers/api/v1/shifts_controller.rb
+++ app/controllers/api/v1/shifts_controller.rb
@@ -3,12 +3,15 @@ class Api::V1::ShiftsController < ApplicationController
 	before_action :set_shift, :only => [:update]
 
 	def index
-		shifts = policy_scope(Shift)
-		render json: shifts, status: :ok
+		query = policy_scope(Shift)
+		query = limit_by_assignment_type(query)
+		query = limit_by_range(query)
+		render json: query, status: :ok
 	end
 
 	def create
 		shift = Shift.new(shift_params)
+		shift.manager_id = current_user.id unless shift.manager_id.present?
 		authorize shift
 		if shift.save
 			render json: shift, status: :ok
@@ -19,7 +22,7 @@ class Api::V1::ShiftsController < ApplicationController
 
 	def update
 		authorize @shift
-		if @shift.update(shift_params)
+		if @shift.update(shift_update_params)
 			render json: @shift, status: :ok
 		else
 			render json: { errors: @shift.errors }, status: :bad_request
@@ -29,11 +32,27 @@ class Api::V1::ShiftsController < ApplicationController
 	private
 
 	def shift_params
+		params.permit(:employee_id, :manager_id, :start_time, :end_time)
+	end
+
+	def shift_update_params
 		params.permit(:employee_id, :start_time, :end_time)
 	end
 
 	def set_shift
 		@shift = Shift.find(params[:id])
 	end
+
+	def limit_by_assignment_type(query)
+		query = query.assigned_to(current_user.id) if params[:scope] == "assigned"
+		query = query.unassigned if params[:scope] == "unassigned"
+		return query
+	end
+
+	def limit_by_range(query)
+		query = query.limit_by_time_range(Time.parse(params[:start]), Time.parse(params[:end])) if params[:start].present? and params[:end].present?
+		return query
+	end
+
 
 end
