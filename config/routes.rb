fe996edf17df17876ecba88da37f8425bfeb9c4f
Max Lund <mlund@billypayments.com>
2017-09-21 15:54:38 -0500

    add shift summary route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
   	namespace :v1 do
   		resources :users
-  		resources :shifts
+  		resources :shifts do
+  			collection do
+  				get :summary
+  			end
+  		end
   	end
 
   end
